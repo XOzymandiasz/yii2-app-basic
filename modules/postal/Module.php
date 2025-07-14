7d7542bb72952bb643fb2599d85d9962dc9aad1c
adam <adam.klamrowski.ak@gmail.com>
2025-07-14 11:03:10 +0200

    feat(poczta-polska): create postal module with translation config and tracker acces
    -Created Module class for poczta-polska -Registered alias @edzima/postal -Configured translation alias

---
+++ modules/postal/Module.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\modules\postal;
+
+use app\modules\postal\components\PocztaPolskaTracker;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\base\Module as BaseModule;
+
+class Module extends BaseModule
+{
+
+    public function init()
+    {
+        parent::init();
+        Yii::setAlias('@edzima/postal', __DIR__);
+        Yii::configure($this, require(__DIR__ . '/config.php'));
+        static::registerTranslations();
+    }
+
+    public static function registerTranslations(): void {
+        Yii::$app->i18n->translations['edzima/postal/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@edzima/postal/messages',
+            'fileMap' => [
+                'edzima/postal/poczta-polska' => 'poczta-polska.php',
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function t($category, $message, $params = [], $language = null): string
+    {
+        return Yii::t('edzima/postal/' . $category, $message, $params, $language);
+    }
+
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function getPocztaPolskaTracker(): PocztaPolskaTracker
+    {
+        return $this->get('pocztaPolskaTracker');
+    }
+}
