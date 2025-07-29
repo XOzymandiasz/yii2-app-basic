124b4aedc0b6900779d2e1eeb8b0a365a5df1315
adam <adam.klamrowski.ak@gmail.com>
2025-07-29 12:38:23 +0200

    feat(shipment): created interfaces for direction and provider const

--- modules/postal/models/ShipmentDirectionInterface.php
+++ modules/postal/models/ShipmentDirectionInterface.php
@@ -4,5 +4,12 @@ namespace app\modules\postal\models;
 
 interface ShipmentDirectionInterface
 {
+    public const DIRECTION_IN = 'IN';
+    public const DIRECTION_OUT = 'OUT';
 
+    public function getDirection(): string;
+
+    public function getDirectionName(): string;
+
+    public static function getDirectionsNames();
 }
--- modules/postal/models/ShipmentProviderInterface.php
+++ modules/postal/models/ShipmentProviderInterface.php
@@ -4,5 +4,16 @@ namespace app\modules\postal\models;
 
 interface ShipmentProviderInterface
 {
+    public const PROVIDER_POCZTA_POLSKA = 'PP';
+    public const PROVIDER_POCZTEX_2021 = 'PXT21';
+    public const PROVIDER_DHL = 'DHL';
+    public const PROVIDER_DPD = 'DPD';
+    public const PROVIDER_GLS = 'GLS';
+    public const PROVIDER_INPOST = 'INPST';
 
+    public function getProvider(): string;
+
+    public function getProviderName(): string;
+
+    public static function getProvidersNames();
 }
