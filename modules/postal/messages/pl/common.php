40ade61a8276bf226aa7f6795abd210f6f66b521
adam <adam.klamrowski.ak@gmail.com>
2025-07-29 12:33:22 +0200

    chore(i18n): split and organize translation files

--- modules/postal/messages/pl/common.php
+++ modules/postal/messages/pl/common.php
@@ -1,1 +1,21 @@
 <?php
+
+return [
+    'Save' => 'Zapisz',
+    'Delete' => 'Usuń',
+    'Update Postal Shipment: {name}' => 'Uaktualnij przesyłkę pocztową: {name}',
+    'In' => 'Wychodząca',
+    'Out' => 'Przychodząca',
+    'Search' => 'Szukaj',
+    'Create Postal Shipment' => 'Utwórz przesyłkę pocztową',
+    'Reset' => 'Resetuj',
+    'Update' => 'Uaktualnij',
+    'Postal Shipments' => 'Przesyłka pocztowa',
+    'Shipment Addresses' => 'Adres pocztowy',
+    'Update Shipment Address: {name}' => 'Uaktualnij adres pocztowy: {name}',
+    'Create Shipment Content' => 'Stwórz zawartość przesyłki',
+    'Are you sure you want to delete this item?' => 'Czy jesteś pewny, że chcesz usunąć ten przedmiot?',
+    'Update Shipment Content: {name}' => 'Uaktualnij zawartość przesyłki: {name}',
+    'Create Shipment Address' => 'Utwórz adres pocztowy',
+    'Shipment Contents' => 'Zawartość przesyłki'
+];
