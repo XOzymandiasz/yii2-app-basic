6d0a9890463644c29f08850d8afd149adba0ed1d
adam <adam.klamrowski.ak@gmail.com>
2025-07-29 12:43:58 +0200

    chore(WSDL): created controller to generate wsdl classes

--- modules/postal/commands/WSDLController.php
+++ modules/postal/commands/WSDLController.php
@@ -2,7 +2,34 @@
 
 namespace app\modules\postal\commands;
 
-class WSDLController
+use WsdlToPhp\PackageGenerator\ConfigurationReader\GeneratorOptions;
+use WsdlToPhp\PackageGenerator\Generator\Generator;
+use Yii;
+use yii\console\Controller;
+
+class WSDLController extends Controller
 {
 
+    public function actionGenerate(): void
+    {
+        $wsdl = 'https://en-testwebapi.poczta-polska.pl/websrv/?wsdl';
+        $destination = Yii::getAlias('@app/modules/postal/sender');
+        $namespace = 'app\\modules\\postal\\sender';
+
+        $options = GeneratorOptions::instance();
+        $options
+            ->setOrigin($wsdl)
+            ->setDestination($destination)
+            ->setNamespace($namespace)
+            ->setCategory('cat')
+            ->setComposerName('poczta-polska-sender')
+            ->setGenerateTutorialFile(false)
+            ->setStandalone(false)
+            ->setNamespaceDictatesDirectories(true);
+
+        $generator = new Generator($options);
+        $generator->generatePackage();
+
+        echo "sukces\n";
+    }
 }
