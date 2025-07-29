95419a3a807685e75686764eea9d4f0c537d4843
adam <adam.klamrowski.ak@gmail.com>
2025-07-29 13:22:36 +0200

    adding migrations with table user

---
+++ migrations/m250728_095723_create_user_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Migration;
+
+class m250728_095723_create_user_table extends Migration
+{
+    public function safeUp(): void
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+        ]);
+    }
+
+    public function safeDown(): void
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
