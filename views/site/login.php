97543d6d0cd4e9ea1976c9b75b1ae15f11a2e727
adamk <adam.klamrowski.ak@gmail.com>
2025-07-31 09:38:47 +0200

    fix(user): created log in option

--- views/site/login.php
+++ views/site/login.php
@@ -11,45 +11,21 @@ use yii\bootstrap5\Html;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<?php
 
-    <p>Please fill out the following fields to login:</p>
+$this->title = 'Logowanie';
 
-    <div class="row">
-        <div class="col-lg-5">
+?>
 
-            <?php $form = ActiveForm::begin([
-                'id' => 'login-form',
-                'fieldConfig' => [
-                    'template' => "{label}\n{input}\n{error}",
-                    'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-                    'inputOptions' => ['class' => 'col-lg-3 form-control'],
-                    'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-                ],
-            ]); ?>
+<h1><?= Html::encode($this->title) ?></h1>
 
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+<?php $form = ActiveForm::begin(); ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+<?= $form->field($model, 'name') ?>
 
-            <?= $form->field($model, 'rememberMe')->checkbox([
-                'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            ]) ?>
-
-            <div class="form-group">
-                <div>
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            </div>
-
-            <?php ActiveForm::end(); ?>
-
-            <div style="color:#999;">
-                You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-                To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-            </div>
-
-        </div>
-    </div>
+<div class="form-group">
+    <?= Html::submitButton('Zaloguj', ['class' => 'btn btn-primary']) ?>
 </div>
+
+<?php ActiveForm::end(); ?>
+
