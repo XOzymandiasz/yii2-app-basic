78753ff2d7771ef0f071b5c665fbd88d0dc59889
adamk <adam.klamrowski.ak@gmail.com>
2025-07-31 14:49:03 +0200

    feat(shipment): add filters for direction provider and content

--- modules/postal/views/shipment/index.php
+++ modules/postal/views/shipment/index.php
@@ -1,7 +1,10 @@
 <?php
 
+use app\modules\postal\forms\ShipmentForm;
 use app\modules\postal\models\Shipment;
+use app\modules\postal\models\ShipmentContent;
 use app\modules\postal\Module;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
@@ -26,17 +29,62 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
 
-    <?= GridView::widget([
+    <?=
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'direction',
+            [
+                'attribute' => 'direction',
+                'filter' => Html::activeDropDownList(
+                    $searchModel,
+                    'direction',
+                    ShipmentForm::getDirectionsNames(),
+                    ['class' => 'form-control', 'prompt' => Module::t('common','-- choose --')]
+                ),
+                'value' => function ($model) {
+                    return ShipmentForm::getDirectionsNames()[$model->direction] ?? $model->direction;
+                },
+                'contentOptions' => ['style' => 'text-align: center; vertical-align: middle;'],
+                'headerOptions' => ['style' => 'text-align: center;'],
+            ],
             'number',
-            'provider',
-            'content_id',
+            [
+                'attribute' => 'provider',
+                'filter' => Html::activeDropDownList(
+                    $searchModel,
+                    'provider',
+                    ShipmentForm::getProvidersNames(),
+                    ['class' => 'form-control', 'prompt' => Module::t('common','-- choose --')]
+                ),
+                'value' => function ($model) {
+                    return ShipmentForm::getProvidersNames()[$model->provider] ?? $model->provider;
+                },
+                'contentOptions' => ['style' => 'text-align: center; vertical-align: middle;'],
+                'headerOptions' => ['style' => 'text-align: center;'],
+            ],
+            [
+                'attribute' => 'content_id',
+                'label' => 'Content',
+                'value' => function ($model) {
+                    return $model->content->name ?? Module::t('common', '(empty)');
+                },
+                'filter' => Html::activeDropDownList(
+                    $searchModel,
+                    'content_id',
+                    ArrayHelper::map(
+                        ShipmentContent::find()->where(['is_active' => 1])->all(),
+                        'id',
+                        'name'
+                    ),
+                    ['class' => 'form-control', 'prompt' => Module::t('common', '-- choose --')]
+                ),
+                'contentOptions' => ['style' => 'text-align: center; vertical-align: middle;'],
+                'headerOptions' => ['style' => 'text-align: center; vertical-align: middle;'],
+            ],
             //'creator_id',
             //'created_at',
             //'updated_at',
