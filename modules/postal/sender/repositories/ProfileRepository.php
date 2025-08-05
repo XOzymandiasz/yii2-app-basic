8c70bfbdd2ed535c7f215ae843742d124c516a35
adamk <adam.klamrowski.ak@gmail.com>
2025-08-05 12:47:44 +0200

    feat(shipment): added getList method which gives ProfileType list from api

--- modules/postal/sender/repositories/ProfileRepository.php
+++ modules/postal/sender/repositories/ProfileRepository.php
@@ -5,6 +5,7 @@ namespace app\modules\postal\sender\repositories;
 use app\modules\postal\sender\services\ProfileService;
 use app\modules\postal\sender\StructType\ProfilType;
 use Yii;
+use yii\helpers\ArrayHelper;
 
 class ProfileRepository extends BaseRepository
 {
@@ -27,6 +28,34 @@ class ProfileRepository extends BaseRepository
         }
         Yii::error($this->getService()->getLastError(), __METHOD__);
         return false;
+    }
+
+    /**
+     * @var ProfilType[]
+     */
+    private array $profiles = [];
+
+    /**
+     * @return ProfilType[]
+     */
+    public function getList(bool $refresh = false): array
+    {
+        if ($refresh || empty($this->profiles)) {
+            $response = $this->getService()->getList();
+            if (!$response) {
+                Yii::warning([
+                    'lastResponseError' => $this->getService()->getLastError()
+                ], __METHOD__);
+            }
+            $this->profiles = ArrayHelper::index($response->getProfil(),
+                function (ProfilType $profile) {
+                    return $profile->getIdProfil();
+                }
+            );
+
+        }
+
+        return $this->profiles;
     }
 
     protected function getService(): ProfileService
