9eb235ecdb50f02e761b93bfb491440f7b45068e
adamk <adam.klamrowski.ak@gmail.com>
2025-08-05 11:54:26 +0200

    refactor(sender-service): created abstract class BaseRepository and refactored classes to inheritance from BaseRepository

--- modules/postal/sender/repositories/BufforRepository.php
+++ modules/postal/sender/repositories/BufforRepository.php
@@ -3,26 +3,17 @@
 namespace app\modules\postal\sender\repositories;
 
 
-use app\modules\postal\sender\PocztaPolskaSenderOptions;
-use app\modules\postal\sender\ServiceType\BufforService;
+use app\modules\postal\sender\services\BufforService;
+use app\modules\postal\sender\StructType\BuforType;
+use app\modules\postal\sender\StructType\PlacowkaPocztowaType;
+use app\modules\postal\sender\StructType\ProfilType;
 use Yii;
-use yii\base\Component;
-use yii\db\Connection;
-use yii\di\Instance;
+use yii\helpers\ArrayHelper;
 
 class BufforRepository extends Component
 {
 
     private ?BufforService $service = null;
-
-    private PocztaPolskaSenderOptions $senderOptions;
-
-
-    public function __construct(PocztaPolskaSenderOptions $senderOptions, array $config = [])
-    {
-        $this->senderOptions = $senderOptions;
-        parent::__construct($config);
-    }
 
 
     public function clear(?int $bufforId): bool
@@ -49,6 +40,71 @@ class BufforRepository extends Component
         }
         return [];
     }
+
+    /**
+     * @var ProfilType[]
+     */
+    private array $profiles = [];
+
+    /**
+     * @return ProfilType[]
+     */
+    public function getProfiles(bool $refresh = false): array
+    {
+        if ($refresh || empty($this->profiles)) {
+            $response = $this->getService()->getProfiles();
+            if (!$response) {
+                Yii::warning([
+                    'lastResponseError' => $this->getService()->getLastError()
+                ], __METHOD__);
+            }
+            $this->profiles = ArrayHelper::index($response->getProfil(),
+                function (ProfilType $profile) {
+                    return $profile->getIdProfil();
+                }
+            );
+
+        }
+
+        return $this->profiles;
+    }
+
+    /**
+     * @return PlacowkaPocztowaType[]
+     */
+    public function getDispatchOffices(string $regionId): array
+    {
+
+        $response = $this->getService()->getPostOffices($regionId);
+        if ($response) {
+            return array_filter($response->getPlacowka(), function (PlacowkaPocztowaType $model): bool {
+                return $model->getIdZPO() === null;
+            });
+        }
+        Yii::warning([
+            'lastResponseError' => $this->getService()->getLastError()
+        ], __METHOD__);
+        return [];
+
+    }
+
+    public function create(BuforType $buffor): bool
+    {
+        $response = $this->getService()->create($buffor);
+
+        if ($response && empty($response->getError())) {
+            return true;
+        }
+
+        Yii::warning($response->getError(), __METHOD__);
+
+        Yii::warning([
+            'lastResponseError' => $this->getService()->getLastError()
+        ], __METHOD__);
+        return false;
+
+    }
+
 
     protected function getService(): BufforService
     {
