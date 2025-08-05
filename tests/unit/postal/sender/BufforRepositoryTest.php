71515858c46134112724d374cd49edba373fb32e
adamk <adam.klamrowski.ak@gmail.com>
2025-08-05 15:25:21 +0200

    test(poczta-polska-buffor): created test for buffor repository

--- tests/unit/postal/sender/BufforRepositoryTest.php
+++ tests/unit/postal/sender/BufforRepositoryTest.php
@@ -2,23 +2,26 @@
 
 namespace unit\postal\sender;
 
+use _support\UnitModelTrait;
 use app\modules\postal\sender\PocztaPolskaSenderOptions;
 use app\modules\postal\sender\repositories\BufforRepository;
-use app\modules\postal\sender\StructType\ProfilType;
+use app\modules\postal\sender\repositories\ProfileRepository;
+use app\modules\postal\sender\StructType\BuforType;
 use Codeception\Test\Unit;
+use edzima\teryt\models\Region;
+use InvalidArgumentException;
 use UnitTester;
-use Yii;
 
 /**
  * @property UnitTester $tester
  */
 class BufforRepositoryTest extends Unit
 {
-
+    private const PROFILE_ID = 772567;
     private BufforRepository $repository;
+    private ?ProfileRepository $profileRepository = null;
 
-
-    public function _before()
+    public function _before(): void
     {
         parent::_before();
         $this->repository = new BufforRepository(
@@ -32,6 +35,58 @@ class BufforRepositoryTest extends Unit
         //$all = Yii::createObject(BufforRepository::class)->getAll();
         codecept_debug($all);
         $this->tester->assertIsArray($all);
+    }
+
+    public function testGetDispatchOffices(): void
+    {
+        $response = $this->repository->getDispatchOffices(Region::REGION_POMORSKIE);
+
+        $this->tester->assertIsArray($response);
+    }
+
+    public function testGetDispatchOfficesWithUknownRegion(): void
+    {
+        $this->tester->expectThrowable(InvalidArgumentException::class, function () {
+            $this->repository->getDispatchOffices('2137');
+        });
+    }
+
+
+    public function testCreate(): void
+    {
+        $bufor = $this->getBuffor(profileId: self::PROFILE_ID);
+
+        $response = $this->repository->create($bufor);
+
+        $this->tester->assertTrue($response);
+    }
+
+
+
+
+    protected function getBuffor(
+        bool $isActive = false,
+        ?string $sendAt = null,
+        ?int $profileId = null,
+        ?int $dispatchOffice = null,
+        ?string $name = null
+    ): BuforType
+    {
+        return (new BuforType)
+                ->setActive($isActive)
+                ->setUrzadNadania($dispatchOffice)
+                ->setOpis($name)
+                ->setDataNadania($sendAt)
+                ->setProfil($this->getProfileRepository()->getList()[$profileId]);
+    }
+
+    private function getProfileRepository(): ?ProfileRepository
+    {
+        $options = PocztaPolskaSenderOptions::testInstance();
+        if (null === $this->profileRepository) {
+            $this->profileRepository = new ProfileRepository($options);
+        }
+        return $this->profileRepository;
     }
 
 }
