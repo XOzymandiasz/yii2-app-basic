96e457db236b1dad544c4cffe23faed55595ee45
adamk <adam.klamrowski.ak@gmail.com>
2025-08-06 15:57:11 +0200

    feat(command): add optional WSDL argument to generation command

--- modules/postal/commands/WSDLController.php
+++ modules/postal/commands/WSDLController.php
@@ -6,13 +6,16 @@ use WsdlToPhp\PackageGenerator\ConfigurationReader\GeneratorOptions;
 use WsdlToPhp\PackageGenerator\Generator\Generator;
 use Yii;
 use yii\console\Controller;
+use yii\helpers\Console;
 
 class WSDLController extends Controller
 {
 
-    public function actionGenerate(): void
+    public function actionGenerate(string $wsdl = null): void
     {
-        $wsdl = 'https://en-testwebapi.poczta-polska.pl/websrv/?wsdl';
+        if($wsdl === null){
+            $wsdl = 'https://en-testwebapi.poczta-polska.pl/websrv/?wsdl';
+        }
         $destination = Yii::getAlias('@app/modules/postal/sender');
         $namespace = 'app\\modules\\postal\\sender';
 
@@ -21,15 +24,12 @@ class WSDLController extends Controller
             ->setOrigin($wsdl)
             ->setDestination($destination)
             ->setNamespace($namespace)
-            ->setCategory('cat')
-            ->setComposerName('poczta-polska-sender')
             ->setGenerateTutorialFile(false)
             ->setStandalone(false)
             ->setNamespaceDictatesDirectories(true);
 
         $generator = new Generator($options);
         $generator->generatePackage();
-
-        echo "sukces\n";
+        Console::output('Success generate.');
     }
 }
