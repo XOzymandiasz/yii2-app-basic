100a86368c826803cc6f56a868f0c1a1f20d25b8
adamk <adam.klamrowski.ak@gmail.com>
2025-08-06 19:28:46 +0200

    refactor(controller): simplify method

--- modules/postal/modules/poczta_polska/sender/ServiceType/Send.php
+++ modules/postal/modules/poczta_polska/sender/ServiceType/Send.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace app\modules\postal\modules\poczta_polska\sender\ServiceType;
 
+use app\modules\postal\modules\poczta_polska\sender\StructType\SendEnvelope;
+use app\modules\postal\modules\poczta_polska\sender\StructType\SendEnvelopeResponseType;
 use SoapFault;
 use WsdlToPhp\PackageBase\AbstractSoapClientBase;
 
@@ -15,25 +17,25 @@ class Send extends AbstractSoapClientBase
 {
     /**
      * Method to call the operation originally named sendEnvelope
-     * @param \app\modules\postal\modules\poczta_polska\sender\StructType\SendEnvelope $parameters
-     * @return \app\modules\postal\modules\poczta_polska\sender\StructType\SendEnvelopeResponseType|bool
+     * @param SendEnvelope $parameters
+     * @return SendEnvelopeResponseType|null
      * @uses AbstractSoapClientBase::saveLastError()
      * @uses AbstractSoapClientBase::getSoapClient()
      * @uses AbstractSoapClientBase::setResult()
      */
-    public function sendEnvelope(\app\modules\postal\modules\poczta_polska\sender\StructType\SendEnvelope $parameters)
+    public function sendEnvelope(?int $idBufor, ?int $urzadNadania, ?array $pakiet): SendEnvelopeResponseType|null
     {
         try {
             $this->setResult($resultSendEnvelope = $this->getSoapClient()->__soapCall('sendEnvelope', [
-                $parameters,
+                new SendEnvelope($urzadNadania, $pakiet, $idBufor),
             ], [], [], $this->outputHeaders));
         
             return $resultSendEnvelope;
         } catch (SoapFault $soapFault) {
             $this->saveLastError(__METHOD__, $soapFault);
-        
-            return false;
         }
+
+        return null;
     }
     /**
      * Returns the result
