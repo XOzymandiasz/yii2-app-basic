662436e8dd9dbcf38a63ebfb0d0492dc6a2f4dfa
adamk <adam.klamrowski.ak@gmail.com>
2025-08-06 10:04:22 +0200

    feat(sender-service-shipment): added printForParcel method

--- modules/postal/sender/services/ShipmentService.php
+++ modules/postal/sender/services/ShipmentService.php
@@ -4,6 +4,9 @@ namespace app\modules\postal\sender\services;
 
 use app\modules\postal\sender\StructType\AddShipment;
 use app\modules\postal\sender\StructType\AddShipmentResponse;
+use app\modules\postal\sender\StructType\GetPrintForParcel;
+use app\modules\postal\sender\StructType\GetPrintForParcelResponse;
+use app\modules\postal\sender\StructType\PrintType;
 use SoapFault;
 
 class ShipmentService extends BaseService
@@ -20,6 +23,22 @@ class ShipmentService extends BaseService
         } catch (SoapFault $soapFault) {
             $this->saveLastError(__METHOD__, $soapFault);
         }
+        return null;
+    }
+
+    public function printForParcel(?array $guid, ?PrintType $type): GetPrintForParcelResponse|null
+    {
+        try {
+            $this->setResult($resultGetPrintForParcel = $this->getSoapClient()->__soapCall('getPrintForParcel', [
+                new GetPrintForParcel($guid, $type),
+            ], [], [], $this->outputHeaders));
+
+            return $resultGetPrintForParcel;
+        } catch (SoapFault $soapFault) {
+            $this->saveLastError(__METHOD__, $soapFault);
+
+        }
+
         return null;
     }
 
