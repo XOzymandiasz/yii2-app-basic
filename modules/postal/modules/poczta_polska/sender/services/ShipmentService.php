b2706a5de39c29afb52eca8c826dc732358093e9
adamk <adam.klamrowski.ak@gmail.com>
2025-08-09 12:36:41 +0200

    feat(shipment-service): added get list of shipments method

--- modules/postal/modules/poczta_polska/sender/services/ShipmentService.php
+++ modules/postal/modules/poczta_polska/sender/services/ShipmentService.php
@@ -4,6 +4,8 @@ namespace app\modules\postal\modules\poczta_polska\sender\services;
 
 use app\modules\postal\modules\poczta_polska\sender\StructType\AddShipment;
 use app\modules\postal\modules\poczta_polska\sender\StructType\AddShipmentResponse;
+use app\modules\postal\modules\poczta_polska\sender\StructType\GetEnvelopeBufor;
+use app\modules\postal\modules\poczta_polska\sender\StructType\GetEnvelopeBuforResponse;
 use app\modules\postal\modules\poczta_polska\sender\StructType\GetPrintForParcel;
 use app\modules\postal\modules\poczta_polska\sender\StructType\GetPrintForParcelResponse;
 use app\modules\postal\modules\poczta_polska\sender\StructType\PrintType;
@@ -39,6 +41,21 @@ class ShipmentService extends BaseService
 
         }
 
+        return null;
+    }
+
+
+    public function getList(int $idBufor): GetEnvelopeBuforResponse|null
+    {
+        try {
+            $this->setResult($resultGetEnvelopeBufor = $this->getSoapClient()->__soapCall('getEnvelopeBufor', [
+                new GetEnvelopeBufor($idBufor),
+            ], [], [], $this->outputHeaders));
+
+            return $resultGetEnvelopeBufor;
+        } catch (SoapFault $soapFault) {
+            $this->saveLastError(__METHOD__, $soapFault);
+        }
         return null;
     }
 
