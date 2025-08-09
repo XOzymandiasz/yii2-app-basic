fd2a47b1f2e0f59a961852b1c0fdf2a669dc65b2
adamk <adam.klamrowski.ak@gmail.com>
2025-08-09 12:55:48 +0200

    test(shipment-repository): tested getting shipment list by idBuffer

--- tests/unit/postal/poczta_polska/sender/repositories/ShipmentRepositoryTest.php
+++ tests/unit/postal/poczta_polska/sender/repositories/ShipmentRepositoryTest.php
@@ -8,8 +8,10 @@ use app\modules\postal\modules\poczta_polska\sender\EnumType\PrintKindEnum;
 use app\modules\postal\modules\poczta_polska\sender\EnumType\PrintMethodEnum;
 use app\modules\postal\modules\poczta_polska\sender\EnumType\PrintResolutionEnum;
 use app\modules\postal\modules\poczta_polska\sender\PocztaPolskaSenderOptions;
+use app\modules\postal\modules\poczta_polska\sender\repositories\BufferRepository;
 use app\modules\postal\modules\poczta_polska\sender\repositories\ShipmentRepository;
 use app\modules\postal\modules\poczta_polska\sender\StructType\AdresType;
+use app\modules\postal\modules\poczta_polska\sender\StructType\BuforType;
 use app\modules\postal\modules\poczta_polska\sender\StructType\PrintType;
 use app\modules\postal\modules\poczta_polska\sender\StructType\PrzesylkaPoleconaKrajowaType;
 use app\modules\postal\modules\poczta_polska\sender\StructType\PrzesylkaType;
@@ -22,6 +24,7 @@ use UnitTester;
 class ShipmentRepositoryTest extends Unit
 {
     private ShipmentRepository $repository;
+    private ?BufferRepository $bufferRepository = null;
 
     public function _before(): void
     {
@@ -31,6 +34,20 @@ class ShipmentRepositoryTest extends Unit
         );
     }
 
+    public function testGetList(): void
+    {
+        $this->bufferRepository = $this->getBufferRepository();
+        /**
+         * @var BuforType[] $buffers
+         */
+        $buffers = $this->bufferRepository->getAll();
+        $firstBuffer = reset($buffers);
+
+        $response = $this->repository->getList($firstBuffer->getIdBufor());
+
+        $this->tester->assertIsArray($response);
+    }
+
     public function testAdd(): void
     {
         $address = $this->getAddress();
@@ -38,9 +55,11 @@ class ShipmentRepositoryTest extends Unit
 
         $response = $this->repository->add($shipment);
 
+        codecept_debug($response->getError());
+
+        $this->tester->assertEmpty($response->getError());
         $this->tester->assertIsString($response->getGuid());
         $this->tester->assertIsString($response->getNumerNadania());
-        $this->tester->assertEmpty($response->getError());
     }
 
     public function testGetLabel(): void
@@ -73,7 +92,7 @@ class ShipmentRepositoryTest extends Unit
             ->setResolution($resolution);
     }
 
-    protected function getShipment(
+    public static function getShipment(
         AdresType $address,
         string $kategoria = KategoriaType::VALUE_PRIORYTETOWA
     ): PrzesylkaType
@@ -82,7 +101,7 @@ class ShipmentRepositoryTest extends Unit
                 ->setAdres($address);
     }
 
-    protected function getAddress(
+    public static function getAddress(
         string $name = 'Test name',
         string $street = 'Test street',
         string $city = 'Test city',
@@ -96,5 +115,13 @@ class ShipmentRepositoryTest extends Unit
             ->setMiejscowosc($city);
     }
 
+    private function getBufferRepository(): ?BufferRepository
+    {
+        $options = PocztaPolskaSenderOptions::testInstance();
+        if (null === $this->bufferRepository) {
+            $this->bufferRepository = new BufferRepository($options);
+        }
+        return $this->bufferRepository;
+    }
 
 }
