d58bba5b89767fe6be6ae39f9d699433010cea9b
adamk <adam.klamrowski.ak@gmail.com>
2025-08-12 15:59:47 +0200

    feat(poczta-polska-profile): added action index

--- modules/postal/modules/poczta_polska/controllers/ProfileController.php
+++ modules/postal/modules/poczta_polska/controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace app\modules\postal\modules\poczta_polska\controllers;
 use app\modules\postal\modules\poczta_polska\forms\ProfileForm;
 use app\modules\postal\modules\poczta_polska\Module;
 use Yii;
+use yii\data\ArrayDataProvider;
 use yii\web\Controller;
 use yii\web\Response;
 
@@ -31,6 +32,28 @@ class ProfileController extends Controller
 
         return $this->render('create', [
             'model' => $model,
+        ]);
+    }
+
+    public function actionIndex():string|Response
+    {
+        $profiles = $this->module
+                ->getRepositoryFactory()
+                ->getProfileRepository()
+                ->getList();
+
+        $dataProvider = new ArrayDataProvider([
+            'allModels' => $profiles,
+            'key' => static function($model) {
+                return $model->getidProfil();
+            },
+            'pagination' => [
+                'pageSize' => 20,
+            ],
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
         ]);
     }
 
