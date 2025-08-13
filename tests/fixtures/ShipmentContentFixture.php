7daf16f004ae4da87fd9545302188cb40a415385
adamk <adam.klamrowski.ak@gmail.com>
2025-08-13 10:07:16 +0200

    test(fixtures): moved fixtures up to tests dir

---
+++ tests/fixtures/ShipmentContentFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace tests\fixtures;
+
+use app\modules\postal\models\ShipmentContent;
+use yii\test\ActiveFixture;
+
+class ShipmentContentFixture extends ActiveFixture
+{
+    public $modelClass = ShipmentContent::class;
+}
