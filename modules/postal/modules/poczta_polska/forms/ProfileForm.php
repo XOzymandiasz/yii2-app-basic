97c9e7fb4a175ff013a817d76fe6b44a80f1d542
adamk <adam.klamrowski.ak@gmail.com>
2025-08-14 09:10:59 +0200

    feat(poczta-polska-profile): add update and setProfileType methods

--- modules/postal/modules/poczta_polska/forms/ProfileForm.php
+++ modules/postal/modules/poczta_polska/forms/ProfileForm.php
@@ -20,7 +20,7 @@ class ProfileForm extends AddressTypeForm
     {
         return array_merge(parent::rules(), [
             [['country'], 'required'],
-            [['idProfil'], 'integer'],
+            [['!idProfil'], 'integer'],
             [['fax', 'profileName'], 'string', 'max' => 30],
             [['mpk'], 'string', 'max' => 50],
         ]);
@@ -36,9 +36,41 @@ class ProfileForm extends AddressTypeForm
         ]);
     }
 
+    /**
+     * @throws InvalidConfigException
+     */
     public function create(ProfileRepository $repository): bool
     {
         return $repository->create($this->createType());
+    }
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function update(ProfileRepository $repository): bool
+    {
+        return $repository->update($this->createType());
+    }
+
+    public function setProfilType(ProfilType $model): void
+    {
+        $this->idProfil = $model->getIdProfil();
+        $this->profileName = $model->getNazwaSkrocona();
+        $this->fax = $model->getFax();
+        $this->mpk = $model->getMpk();
+        $this->name  = $model->getNazwa();
+        $this->street = $model->getUlica();
+        $this->house_number = $model->getNumerDomu();
+        $this->apartment_number = $model->getNumerLokalu();
+        $this->city = $model->getMiejscowosc();
+        $this->postal_code = $model->getKodPocztowy();
+        $this->country = $model->getKraj();
+        $this->name_2 = $model->getNazwa2();
+        $this->phone = $model->getTelefon();
+        $this->email = $model->getEmail();
+        $this->mobile = $model->getMobile();
+        $this->contact_person = $model->getOsobaKontaktowa();
+        $this->taxID = $model->getNip();
     }
 
     protected function createType(): ProfilType
