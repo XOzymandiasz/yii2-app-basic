b0041c1008a21ab921892a9fd3da7d2828f4fe79
adamk <adam.klamrowski.ak@gmail.com>
2025-08-18 20:36:30 +0200

    feat(migration): added creating user

--- migrations/m250728_095723_create_user_table.php
+++ migrations/m250728_095723_create_user_table.php
@@ -11,6 +11,11 @@ class m250728_095723_create_user_table extends Migration
             'name' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(255)->defaultValue(null),
         ]);
+
+        $this->insert('{{%user}}', [
+            'name' => 'admin',
+            'auth_key' => Yii::$app->security->generateRandomString(),
+        ]);
     }
 
     public function safeDown(): void
