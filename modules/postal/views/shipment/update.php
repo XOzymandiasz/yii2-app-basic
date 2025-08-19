b82b0edad477e1ea6f928e2144b6d9e9af9e8825
adamk <adam.klamrowski.ak@gmail.com>
2025-08-19 12:06:14 +0200

    style(shipment): deleted whitespaces

--- modules/postal/views/shipment/update.php
+++ modules/postal/views/shipment/update.php
@@ -4,13 +4,13 @@ use app\modules\postal\Module;
 use yii\helpers\Html;
 
 /** @var yii\web\View $this */
-/** @var app\modules\postal\models\Shipment $model */
+/** @var app\modules\postal\forms\ShipmentForm $model */
 
 $this->title = Module::t('postal', 'Update Postal Shipment: {name}', [
-    'name' => $model->id,
+    'name' => $model->getModel()->id,
 ]);
 $this->params['breadcrumbs'][] = ['label' => Module::t('common', 'Postal Shipments'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->getModel()->id, 'url' => ['view', 'id' => $model->getModel()->id]];
 $this->params['breadcrumbs'][] = Module::t('common', 'Update');
 ?>
 <div class="postal-shipment-update">
