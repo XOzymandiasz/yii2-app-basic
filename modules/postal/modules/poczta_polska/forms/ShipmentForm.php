d3405c5d9d8087e84869f9dd4d6031d9993aad24
adamk <adam.klamrowski.ak@gmail.com>
2025-08-19 11:59:24 +0200

    feat(shipment): added update method for shipments

--- modules/postal/modules/poczta_polska/forms/ShipmentForm.php
+++ modules/postal/modules/poczta_polska/forms/ShipmentForm.php
@@ -4,15 +4,17 @@ namespace app\modules\postal\modules\poczta_polska\forms;
 
 use app\modules\postal\forms\ShipmentForm as PostalShipmentForm;
 use app\modules\postal\models\Shipment;
-use app\modules\postal\Module as PostalModule;
+use app\modules\postal\Module as BaseModule;
 use app\modules\postal\modules\poczta_polska\builders\PocztaPolskaCreateShipmentFactory;
 use app\modules\postal\modules\poczta_polska\repositories\BufferRepository;
 use app\modules\postal\modules\poczta_polska\repositories\ShipmentRepository;
 use app\modules\postal\modules\poczta_polska\sender\EnumType\FormatType;
 use app\modules\postal\modules\poczta_polska\sender\EnumType\KategoriaType;
 use app\modules\postal\modules\poczta_polska\sender\StructType\BuforType;
+use app\modules\postal\modules\poczta_polska\sender\StructType\PrzesylkaPoleconaKrajowaType;
 use app\modules\postal\modules\poczta_polska\sender\StructType\PrzesylkaType;
 use Throwable;
+use yii\base\InvalidConfigException;
 use yii\db\StaleObjectException;
 use yii\helpers\ArrayHelper;
 
@@ -24,7 +26,7 @@ class ShipmentForm extends PostalShipmentForm
     protected const MASS_DEFAULT = 500;
 
     public bool $isRegistered = true;
-    public ?int $idBuffer = null;
+    public ?int $bufferId = null;
     public ?string $description = null;
 
     public ?int $mass = self::MASS_DEFAULT;
@@ -41,11 +43,11 @@ class ShipmentForm extends PostalShipmentForm
     public function rules(): array
     {
         return [
-            [['category', 'idBuffer'], 'required'],
+            [['category', 'bufferId'], 'required'],
             [['category', 'format'], 'string'],
             [['isRegistered'], 'boolean'],
-            [['idBuffer', 'mass'], 'integer'],
-            [['idBuffer'], 'in', 'range' => array_keys($this->getBufforsNames())],
+            [['bufferId', 'mass'], 'integer'],
+            [['bufferId'], 'in', 'range' => array_keys($this->getBufforsNames())],
             [['category'], 'in', 'range' => array_keys(self::getCategoriesNames())],
             [['format'], 'in', 'range' => array_keys(self::getFormatTypes())],
             [['description'], 'string', 'max' => 500],
@@ -55,12 +57,13 @@ class ShipmentForm extends PostalShipmentForm
     public function attributeLabels(): array
     {
         return [
-            'category' => PostalModule::t('poczta-polska', 'Category'),
-            'format' => PostalModule::t('poczta-polska', 'Format'),
-            'description' => PostalModule::t('poczta-polska', 'Description'),
-            'shipperAddressForm' => PostalModule::t('poczta-polska', 'Shippeer Address'),
-            'isRegistered' => PostalModule::t('poczta-polska', 'Registered'),
-            'mass' => PostalModule::t('poczta-polska', 'Mass'),
+            'category' => BaseModule::t('poczta-polska', 'Category'),
+            'format' => BaseModule::t('poczta-polska', 'Format'),
+            'description' => BaseModule::t('poczta-polska', 'Description'),
+            'shipperAddressForm' => BaseModule::t('poczta-polska', 'Shipper Address'),
+            'isRegistered' => BaseModule::t('poczta-polska', 'Registered'),
+            'mass' => BaseModule::t('poczta-polska', 'Mass'),
+            'bufferId' => BaseModule::t('poczta-polska', 'Buffer'),
         ];
     }
 
@@ -69,21 +72,74 @@ class ShipmentForm extends PostalShipmentForm
      * @throws Throwable
      * @throws StaleObjectException
      */
-    public function addShipment(ShipmentRepository $repository): bool
+    public function add(ShipmentRepository $repository): bool
     {
-        $response = $repository->add($this->createShipment(), $this->idBuffer);
+        $response = $repository->add($this->createShipment(), $this->bufferId);
 
         if ($response === null) {
             return false;
         }
 
         $model = $this->getModel();
+        $model->buffer_id = $this->bufferId;
         $model->guid = $response->getGuid();
         $model->number = $response->getNumerNadania();
 
         $model->update(false);
 
         return true;
+    }
+
+    /**
+     * @throws Throwable
+     * @throws InvalidConfigException
+     */
+    public function update(ShipmentRepository $repository, int $bufferId): bool
+    {
+        $shipmentCopy = $repository->getOne($bufferId, $this->getModel()->guid, true);
+
+        $this->number = '';
+
+        if ($shipmentCopy === null) {
+            return false;
+        }
+
+        $shipment = $this->createShipment();
+
+        if ($this->getModel()->guid === null) {
+            return false;
+        }
+
+        $shipment->setGuid($this->getModel()->guid);
+
+        if (!$repository->clear($bufferId, $shipment->getGuid())){
+            return false;
+        }
+
+        $response = $repository->add($shipment, $this->bufferId);
+        if ($response === null) {
+            $repository->add($shipmentCopy, $bufferId);
+            return false;
+        }
+
+        $model = $this->getModel();
+        $model->number = $response->getNumerNadania();
+        $model->guid = $response->getGuid();
+        $model->buffer_id = $this->buffer_id;
+
+        $model->update(false);
+
+
+        return true;
+    }
+
+    public function setShipment(PrzesylkaType $model): void
+    {
+        $this->format = $model->getFormat();
+        $this->mass = $model->getMasa();
+        $this->category = $model->getKategoria();
+        $this->isRegistered = $model instanceof PrzesylkaPoleconaKrajowaType;
+        $this->description = $model->getOpis();
     }
 
     public function createShipment(): PrzesylkaType
@@ -118,17 +174,17 @@ class ShipmentForm extends PostalShipmentForm
     public static function getCategoriesNames(): array
     {
         return [
-            KategoriaType::VALUE_EKONOMICZNA => PostalModule::t('poczta-polska', 'Category: Economies'),
-            KategoriaType::VALUE_PRIORYTETOWA => PostalModule::t('poczta-polska', 'Category: Priority'),
+            KategoriaType::VALUE_EKONOMICZNA => BaseModule::t('poczta-polska', 'Category: Economies'),
+            KategoriaType::VALUE_PRIORYTETOWA => BaseModule::t('poczta-polska', 'Category: Priority'),
         ];
     }
 
     public static function getFormatTypes(): array
     {
         return [
-            FormatType::VALUE_S => PostalModule::t('poczta-polska', 'S'),
-            FormatType::VALUE_M => PostalModule::t('poczta-polska', 'M'),
-            FormatType::VALUE_L => PostalModule::t('poczta-polska', 'L'),
+            FormatType::VALUE_S => BaseModule::t('poczta-polska', 'S'),
+            FormatType::VALUE_M => BaseModule::t('poczta-polska', 'M'),
+            FormatType::VALUE_L => BaseModule::t('poczta-polska', 'L'),
         ];
     }
 
