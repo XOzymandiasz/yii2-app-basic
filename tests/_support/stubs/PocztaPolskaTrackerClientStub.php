33f6ed0d2fc25a04b3f45967d0ac59d713664716
adamk <adam.klamrowski.ak@gmail.com>
2025-08-25 21:44:16 +0200

    test(poczta_polska): created stubs to improve poczta-polska/module tests

---
+++ tests/_support/stubs/PocztaPolskaTrackerClientStub.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tests\_support\stubs;
+
+use app\modules\postal\modules\poczta_polska\components\PocztaPolskaTrackerClient;
+
+final class PocztaPolskaTrackerClientStub extends PocztaPolskaTrackerClient
+{
+
+}
